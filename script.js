2bf64767274e9494b0eab13d38d4f9877fe22f0f
naliniseenan-oss <naliniseenan@gmail.com>
2025-09-12 17:11:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form submission alert
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Thank you for reaching out! I will get back to you soon.");
+  this.reset();
+});
